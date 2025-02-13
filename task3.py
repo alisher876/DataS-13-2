f24f9ab07f11fa728c23cfb4861c64db76a3c1c6
alisher876 <a.kurashev@newuu.uz>
2025-02-13 22:02:23 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -9,3 +9,22 @@ def decorator_1(func):
         print(f"Function '{func.__name__}' executed in {execution_time:.4f} seconds")
         return result
     return wrapper
+
+
+@decorator_1
+def func():
+    print("I am ready to Start")
+    result = 0
+    n =  random.randint(10,751)
+    for i in range(n):
+        result += (i**2)
+        
+@decorator_1
+def funx(n=2, m=5):
+    print("I am ready to do serious stuff")
+    max_val = float('-inf')
+    n =  random.randint(10,751)
+    res = [pow(i,2) for i in range(n)]
+    for i in res:
+        if i > max_val: 
+            max_val = i
