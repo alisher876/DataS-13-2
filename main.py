837157ddedbc729db117088135195b7587228e59
alisher876 <a.kurashev@newuu.uz>
2025-02-13 22:01:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from task1 import kwargsAcceptFun
+from task2 import typeBasedTransformer
+from task3 import decorator_1
+import random
+
+
+
+
+kwargsAcceptFun(name='A', age=20, occupation='Student  ')
+
+
+a = typeBasedTransformer(
+    n=4, t="Hello"
+)
+print(a)
+
+
+
+
+
+
+func() 
+funx() 
+func() 
+funx() 
+func()
+
+
+
 
